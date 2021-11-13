a9a89b500df18f524446d8a10f850be6375ae9f4
lazySia <tldk9640@naver.com>
2021-11-13 16:32:47 +0900

    tab2-animation

--- src/Detail.js
+++ src/Detail.js
@@ -4,6 +4,7 @@ import { Nav } from 'react-bootstrap';
 import styled from 'styled-components';
 import './Detail.scss';
 import {재고context} from './App.js';
+import { CSSTransition } from 'react-transition-group';
 
 let 박스 = styled.div`
   padding : 20px;
@@ -33,6 +34,7 @@ function Detail(props) {
   // let [inputData, inputData변경] = useState('');
 
   let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
 
   let 재고 = useContext(재고context);
 
@@ -79,19 +81,35 @@ function Detail(props) {
 
         <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
           <Nav.Item>
-            <Nav.Link eventKey="link-0">Active</Nav.Link>
+            <Nav.Link eventKey="link-0" onClick={()=>{ 스위치변경(false); 누른탭변경(0)}}>Active</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+            <Nav.Link eventKey="link-1" onClick={()=>{ 스위치변경(false); 누른탭변경(1)}}>Option 2</Nav.Link>
           </Nav.Item>
         </Nav>
 
-        <div></div>
-        <div></div>
-        <div></div>
+        <CSSTransition in={스위치} classNames="wow" timeout={500}>
+         <TabContent 누른탭={누른탭} 스위치변경={스위치변경}/>
+        </CSSTransition>
 
       </div> 
     )
+}
+
+function TabContent(props) {
+
+  useEffect(()=>{
+    props.스위치변경(true);
+  })
+  
+  if (props.누른탭 === 0) {
+    return <div>0번째 내용입니다.</div> 
+  } else if (props.누른탭 === 1) {
+    return <div>1번째 내용입니다.</div>
+  } else if (props.누른탭 === 2) {
+    return <div>2번째 내용입니다.</div>
+  }
+
 }
 
 function Info(props) {
